75c54ea8144509cd659713ab5669c96bdc021712
prcryx <me.prc.fo@gmail.com>
2023-09-27 04:25:38 +0530

    fix: updating base condition

--- create_binary_tree/main.go
+++ create_binary_tree/main.go
@@ -17,10 +17,9 @@ func CreateFirstNode(data int) *Node {
 }
 
 func (node *Node) Insert(data int) {
-	if node == nil {
+	if node == nil || data == -999 {
 		return
 	} else {
-
 		if node.data >= data {
 			//add data in left node
 			if node.left == nil {
@@ -49,14 +48,10 @@ func (node *Node) Insert(data int) {
 
 func (root *Node) Display() {
 	fmt.Printf("    %v    \n", root.data)
-	fmt.Printf("    /\\    \n")
-	fmt.Printf("   %v %v    \n", root.left.data, root.right.data)
-	fmt.Printf("  /   \n")
-	fmt.Printf(" %v   \n", root.left.left.data)
-	fmt.Printf("  \\   \n")
-	fmt.Printf("  %v   \n", root.left.left.right.data)
-	fmt.Printf("   \\   \n")
-	fmt.Printf("    %v   \n", root.left.left.right.right.data)
+	fmt.Printf("   / \\    \n")
+	fmt.Printf("  %v   %v   \n", root.left.data, root.right.data)
+	fmt.Printf(" / \\        \n")
+	fmt.Printf("%v   %v        \n", root.left.left.data, root.left.right.data)
 }
 
 func main() {
@@ -65,9 +60,10 @@ func main() {
 
 	root.Insert(20)
 	root.Insert(5)
-	root.Insert(1)
+
 	root.Insert(3)
-	root.Insert(4)
+	root.Insert(-999)
+	root.Insert(6)
 
 	root.Display()
 }
